0169cbbabbc49504cfaa8f520dd275a2e0eb16e8
rubencelislobato@gmail.com <rubencelislobato@gmail.com>
2023-10-27 10:02:33 +0200

    Cambios realizados numeros_especiales.py y su test

--- src/numero_especiales.py
+++ src/numero_especiales.py
@@ -1,19 +1,19 @@
 
-def sumaParesimpares(numInicial, nunFinal):
+def sumaParesimpares(numInicial, numFinal):
     
-    if numInicial and numFinal %3 == 0:
-        for numero in range(numInicial, numFinal):
-            suma = numero + numero
-            contador += 1
-    else:
-        for numero in range(numInicial, numFinal):
-            suma = numero + numero
-            contador += 1
+    for numero in range(numInicial, numFinal + 1):
+        if numInicial % 2 == 0:
+            if parImpar.lower() == "pares" and numero % 2 == 0:
+                suma += numero
+            elif parImpar.lower() == "impares" and numero % 2 != 0:
+                suma += numero
+        numInicial += 1
     return str(suma)
 
 
 if __name__ == "__main__":
     # Entrda
+    suma = 0
     contador = 0
     listaNumeros = []
     numInicial = int(input("Ingrese el número inicial: "))
